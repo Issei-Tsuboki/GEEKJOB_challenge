24c8ab99094599f6fb8a6b60709807b3284ceae0
TSUBOKI-PC\issei <issei.yui.ccs@gmail.com>
2017-05-23 14:27:18 +0900

    引数、戻り値1

---
+++ CampChallenge_java/004.JSPとサーブレット２/JavaChallenge1501.java
@@ -0,0 +1,117 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author issei
+ */
+@WebServlet(urlPatterns = {"/JavaChallenge1501"})
+public class JavaChallenge1501 extends HttpServlet {
+    
+     String[] data(int id){
+         
+          String[] data1 = {"1","名前1","生年月日1","住所1"};
+         
+       switch(id){
+           case 2:
+               data1[0]="2";
+               data1[1]="名前2";
+               data1[2]="生年月日2";
+               data1[3]="住所2";
+               break;
+           case 3:
+               data1[0]="3";
+               data1[1]="名前3";
+               data1[2]="生年月日3";
+               data1[3]="住所3";
+               break;
+           
+       }
+       return data1;
+     }
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+           
+            int id =1;
+            
+            String[] data1 = data(id);
+            out.println(data1[1]);
+            out.println(data1[2]);
+            out.println(data1[3]);
+            
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet JavaChallenge1501</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet JavaChallenge1501 at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
