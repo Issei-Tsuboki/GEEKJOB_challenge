542410e05db554365358f69c009de37e4811032b
TSUBOKI-PC\issei <issei.yui.ccs@gmail.com>
2017-05-30 10:20:50 +0900

    クラスの作成

---
+++ CampChallenge_java/012.クラス/JavaChallenge1701.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author issei
+ */
+public class JavaChallenge1701 {
+    class Number{
+        public int one = 0;
+        public int two = 0;
+        
+        public void setNumber(int o,int t){
+            this.one = o;
+            this.two = t;
+        }
+        public void printNumber(){
+            System.out.println(one);
+            System.out.println(two);
+        }
+    }
+    
+}
