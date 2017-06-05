df336abbe7acf559e1693d47fb76a1298f01e68f
TSUBOKI-PC\issei <issei.yui.ccs@gmail.com>
2017-06-05 14:59:23 +0900

    文字の入れ替え

---
+++ CampChallenge_java/014.標準クラス/moji03.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package moji;
+
+/**
+ *
+ * @author issei
+ */
+public class moji03 {
+    public static void main(String[] args){
+        String moji = "きょUはぴIえIちぴIのくみこみかんすUのがくしゅUをしてIます";
+        
+        System.out.println(moji = moji.replace("I", "い"));
+        
+        System.out.println(moji.replace("U", "う"));
+    }
+}
