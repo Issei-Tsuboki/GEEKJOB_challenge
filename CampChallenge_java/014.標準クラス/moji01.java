57ac6fcb87e163f6ea5e316018ba81e3e9dfd886
TSUBOKI-PC\issei <issei.yui.ccs@gmail.com>
2017-06-05 13:43:11 +0900

    バイト数と文字数の取得

---
+++ CampChallenge_java/014.標準クラス/moji01.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package moji;
+
+/**
+ *
+ * @author issei
+ */
+public class moji01 {
+    public static void main(String[] args){
+        String name = "坪木一誠";
+        
+        System.out.print(name.getBytes().length);
+        System.out.print(name.length());
+    }
+}
